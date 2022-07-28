dbba67d1abf239eda18304aa407a6acdc088cb9d
Jepp0103 <jeppendyekjaer@hotmail.com>
2022-07-28 21:46:36 +0200

    Created post request for inserting track and refactored database class

--- MinderApi/MinderApi/Controllers/TrackController.cs
+++ MinderApi/MinderApi/Controllers/TrackController.cs
@@ -1,26 +1,51 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MinderApi.Models;
-using System.Data;
+using MySql.Data.MySqlClient;
 
 namespace MinderApi.Controllers
 {
-    [Route("api/[controller]")]
+    [Produces("application/json")]
+    [Route("api/tracks")]
     [ApiController]
     public class TrackController : ControllerBase
     {
         private readonly IConfiguration Configuration;
-        private readonly DatabaseConnection dbConnection;
+        private readonly ChinookDatabase dbConnection;
 
         public TrackController(IConfiguration configuration)
         {
             Configuration = configuration;
-            dbConnection = new DatabaseConnection(Configuration);
+            dbConnection = new ChinookDatabase(Configuration);
         }
 
         [HttpGet]
         public JsonResult GetTracks() {
-            DataTable trackTable = dbConnection.ExecuteSQLQuery("SELECT name FROM track;");
+            string query = "SELECT `name` FROM `track`;";
+            var trackTable = dbConnection.SelectSQLQuery(query);
             return new JsonResult(trackTable);
         }
-    }
+
+        [HttpPost]
+        public JsonResult AddTrack([FromBody] Track trackModel) {
+            string insertionQuery = @"
+                INSERT INTO `track` (`Name`, `MediaTypeId`, `Composer`, `Milliseconds`, `UnitPrice`)
+                VALUES(@Name, @MediaTypeId, @Composer, @Milliseconds, @UnitPrice);
+            ";
+
+            var insertedTrackId = dbConnection.InsertSQLQuery(insertionQuery, new[]
+                {
+                    new MySqlParameter("@Name", trackModel.Name),
+                    new MySqlParameter("@MediaTypeId", trackModel.MediaTypeId),
+                    new MySqlParameter("@Composer", trackModel.Composer),
+                    new MySqlParameter("@Milliseconds", trackModel.MilliSeconds),
+                    new MySqlParameter("@UnitPrice", trackModel.UnitPrice)
+                }
+            );
+
+            string selectQuery = @"SELECT * FROM `track` WHERE TrackId = @TrackId";
+            var response = dbConnection.SelectSQLQueryById(selectQuery, "@TrackId", insertedTrackId);
+
+            return new JsonResult(response);
+        }
+    }   
 }
--- MinderApi/MinderApi/Models/Track.cs
+++ MinderApi/MinderApi/Models/Track.cs
@@ -1,10 +1,12 @@
 ﻿namespace MinderApi.Models
 {
-    public class Track
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Composer { get; set; }
+    public class Track { 
+    
+        public string? Name { get; set; }
+        public string? Composer { get; set; }        
+        public int MediaTypeId { get; set; }
+        public int MilliSeconds { get; set; }
+        public int UnitPrice { get; set; }
 
     }
 }
