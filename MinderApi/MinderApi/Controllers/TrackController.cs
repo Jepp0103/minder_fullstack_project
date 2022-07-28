e75df9ac5b620987076b8750ec2bc9b43a84d1c8
Jepp0103 <jeppendyekjaer@hotmail.com>
2022-07-28 13:24:12 +0200

    Git ignore change

--- MinderApi/MinderApi/Controllers/TrackController.cs
+++ MinderApi/MinderApi/Controllers/TrackController.cs
@@ -1,11 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using MySql.Data.MySqlClient;
-using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using MinderApi.Models;
 using System.Data;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace MinderApi.Controllers
 {
@@ -14,32 +9,17 @@ namespace MinderApi.Controllers
     public class TrackController : ControllerBase
     {
         private readonly IConfiguration Configuration;
+        private readonly DatabaseConnection dbConnection;
 
         public TrackController(IConfiguration configuration)
         {
             Configuration = configuration;
+            dbConnection = new DatabaseConnection(Configuration);
         }
 
         [HttpGet]
         public JsonResult GetTracks() {
-            string serverStr = Configuration["Credentials:Server"];
-            string userStr = Configuration["Credentials:User"];
-            string databaseStr = Configuration["Credentials:Database"];
-            string portStr = Configuration["Credentials:Port"];
-            string passwordStr = Configuration["Credentials:Password"];
-            string ConnectionString = $"server={serverStr};user={userStr};database={databaseStr};port={portStr};password={passwordStr}";
-
-            MySqlConnection connection = new MySqlConnection(ConnectionString);
-            connection.Open();
-            MySqlCommand trackCommand = new MySqlCommand("SELECT name FROM track;", connection);
-            MySqlDataReader trackReader = trackCommand.ExecuteReader();
-            DataTable trackTable = new DataTable();
-            trackTable.Load(trackReader);
-
-            trackReader.Close();
-            trackCommand.Dispose();
-            connection.Close();
-
+            DataTable trackTable = dbConnection.ExecuteSQLQuery("SELECT name FROM track;");
             return new JsonResult(trackTable);
         }
     }
