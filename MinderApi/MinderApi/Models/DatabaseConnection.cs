3717175d6545a987f173fb8a16a774c1c0a736fa
Jepp0103 <jeppendyekjaer@hotmail.com>
2022-07-28 13:00:31 +0200

    API and connection to db refactored

---
+++ MinderApi/MinderApi/Models/DatabaseConnection.cs
@@ -0,0 +1,43 @@
+﻿using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace MinderApi.Models
+{
+    public class DatabaseConnection
+    {
+        private readonly IConfiguration Configuration;
+
+        public DatabaseConnection(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public MySqlConnection ConnectToDB()
+        {
+            string serverStr = Configuration["Credentials:Server"];
+            string userStr = Configuration["Credentials:User"];
+            string databaseStr = Configuration["Credentials:Database"];
+            string portStr = Configuration["Credentials:Port"];
+            string passwordStr = Configuration["Credentials:Password"];
+            string ConnectionString = $"server={serverStr};user={userStr};database={databaseStr};port={portStr};password={passwordStr}";
+
+            MySqlConnection connection = new MySqlConnection(ConnectionString);
+            connection.Open();
+
+            return connection;
+        }
+
+        public DataTable ExecuteSQLQuery(string sqlCommand) { 
+            var connection = ConnectToDB();
+            MySqlCommand command = new MySqlCommand(sqlCommand, connection);
+            MySqlDataReader reader = command.ExecuteReader();
+            DataTable table = new DataTable();
+            table.Load(reader);
+            reader.Close();
+            command.Dispose();
+            connection.Close();
+
+            return table;
+        }
+    }
+}
